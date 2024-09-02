e70a92648984dd3d4991c10f4a8dd0a763996332
tAf <muzirat0@gmail.com>
2024-09-02 13:36:53 +0200

    get with standard promis

--- utils/redis.js
+++ utils/redis.js
@@ -1,33 +1,39 @@
 import { createClient } from 'redis';
 
 class RedisClient {
-	constructor () {
-		this.client = createClient().on('error', (error) => console.log(error));
-	}
+  constructor() {
+	this.client = createClient().on('error', (error) => console.log(error));
+  }
 
-	isAlive () {
-		if (this.client) {
-			return true;
-		} else {
-			return false;
-		}
+  isAlive() {
+	if (this.client) {
+		return true;
+	} else {
+		return false;
 	}
+  }
 
-	async get (key) {
-		const val = await this.client.get(key);
-		if (!val) {
-			return null;
-		}
-		return val;
-	}
+  async get(key) {
+	return new Promise((resolve, reject) => {
+		this.client.get(key, (error, val) => {
+			if (error) {
+				return reject(error);
+			}
+			if (val != null) {
+				return resolve(val);
+			}
+			return resolve(null);
+		});
+	});
+  }
 
-	async set (key, val, duration) {
-		await this.client.set(key, val, 'EX', duration);
-	}
+  async set(key, val, duration) {
+	await this.client.set(key, val, 'EX', duration);
+  }
 
-	async del (key) {
-		return await this.client.del(key);
-	}
+  async del(key) {
+	return await this.client.del(key);
+  }
 }
 
 const redisClient = new RedisClient();
